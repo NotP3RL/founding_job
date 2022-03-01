dccff3504c9fa37f44362ee0cf7207e5e0995afe
NotP3RL <ivan.novikov301208@gmail.com>
2022-03-01 16:59:31 +0300

    split composite funcs

--- hhru.py
+++ hhru.py
@@ -5,36 +5,41 @@ import requests
 from tools import predict_salary
 
 
+def get_language_hh_vacancies(language):
+    sum_salary = 0
+    vacancies_processed = 0
+    for page_number in count(0, 1):
+        params = {
+            'specialization': '1.221',  # id of programmer specialization
+            'area': '1',  # moscow area id
+            'period': '30',
+            'per_page': '100',
+            'page': page_number,
+            'text': language
+        }
+        response = requests.get('https://api.hh.ru/vacancies', params=params)
+        response.raise_for_status()
+        formated_response = response.json()
+        for vacancy in formated_response['items']:
+            if vacancy['salary'] and vacancy['salary']['currency'] == 'RUR':
+                sum_salary += predict_salary(
+                    vacancy['salary']['from'],
+                    vacancy['salary']['to']
+                )
+                vacancies_processed += 1
+        if page_number >= formated_response['pages'] - 1:
+            break
+    language_vacancies = {
+        'vacancies_found': formated_response['found'],
+        'vacancies_processed': vacancies_processed,
+        'average_salary': int(sum_salary / vacancies_processed)
+    }
+    return language_vacancies
+
+
 def get_hh_vacancies(languages):
     vacancies = {}
 
     for language in languages:
-        sum_salary = 0
-        vacancies_processed = 0
-        for page_number in count(0, 1):
-            params = {
-                'specialization': '1.221', # id of programmer specialization
-                'area': '1', # moscow area id
-                'period': '30',
-                'per_page': '100',
-                'page': page_number,
-                'text': language
-            }
-            response = requests.get('https://api.hh.ru/vacancies', params=params)
-            response.raise_for_status()
-            formated_response = response.json()
-            for vacancy in formated_response['items']:
-                if vacancy['salary'] and vacancy['salary']['currency'] == 'RUR':
-                    sum_salary += predict_salary(
-                        vacancy['salary']['from'],
-                        vacancy['salary']['to']
-                    )
-                    vacancies_processed += 1
-            if page_number >= formated_response['pages'] - 1:
-                break
-        vacancies[language] = {
-            'vacancies_found': formated_response['found'],
-            'vacancies_processed': vacancies_processed,
-            'average_salary': int(sum_salary / vacancies_processed)
-        }
+        vacancies[language] = get_language_hh_vacancies(language)
     return vacancies
--- superjob.py
+++ superjob.py
@@ -6,47 +6,54 @@ import requests
 from tools import predict_salary
 
 
-def get_sj_vacancies(languages, token):
-    headers = {
-        'X-Api-App-Id': token
+def get_language_sj_vacancies(language, token, vacancy_count_per_page):
+    headers = {'X-Api-App-Id': token}
+    sum_salary = 0
+    vacancies_processed = 0
+    for page_number in count(0, 1):
+        params = {
+            'town': 'Moscow',
+            'catalogues': '48',  # id of programmer specialization
+            'keyword': language,
+            'count': vacancy_count_per_page,
+            'page': page_number
+        }
+        response = requests.get(
+            'https://api.superjob.ru/2.0/vacancies/',
+            headers=headers,
+            params=params
+        )
+        response.raise_for_status()
+        formated_response = response.json()
+        for vacancy in formated_response['objects']:
+            if vacancy['currency'] == 'rub':
+                if vacancy['payment_from'] or vacancy['payment_to']:
+                    sum_salary += predict_salary(
+                        vacancy['payment_from'],
+                        vacancy['payment_to']
+                    )
+                    vacancies_processed += 1
+        vacancy_page_count = ceil(
+            formated_response['total'] / vacancy_count_per_page
+        )
+        if page_number == vacancy_page_count - 1:
+            break
+    language_vacancies = {
+        'vacancies_found': formated_response['total'],
+        'vacancies_processed': vacancies_processed,
+        'average_salary': int(sum_salary / vacancies_processed)
     }
+    return language_vacancies
+
+
+def get_sj_vacancies(languages, token):
     vacancies = {}
 
     vacancy_count_per_page = 100
     for language in languages:
-        sum_salary = 0
-        vacancies_processed = 0
-        for page_number in count(0, 1):
-            params = {
-                'town': 'Moscow',
-                'catalogues': '48', # id of programmer specialization
-                'keyword': language,
-                'count': vacancy_count_per_page,
-                'page': page_number
-            }
-            response = requests.get(
-                'https://api.superjob.ru/2.0/vacancies/',
-                headers=headers,
-                params=params
-            )
-            response.raise_for_status()
-            formated_response = response.json()
-            for vacancy in formated_response['objects']:
-                if vacancy['currency'] == 'rub':
-                    if vacancy['payment_from'] or vacancy['payment_to']:
-                        sum_salary += predict_salary(
-                            vacancy['payment_from'],
-                            vacancy['payment_to']
-                        )
-                        vacancies_processed += 1
-            vacancy_page_count = ceil(
-                formated_response['total'] / vacancy_count_per_page
-            )
-            if page_number == vacancy_page_count - 1:
-                break
-        vacancies[language] = {
-            'vacancies_found': formated_response['total'],
-            'vacancies_processed': vacancies_processed,
-            'average_salary': int(sum_salary / vacancies_processed)
-        }
+        vacancies[language] = get_language_vacancies(
+            language,
+            token,
+            vacancy_count_per_page
+        )
     return vacancies

aa5d53e0feac3b0ab68733b1720877203c2c7e68
NotP3RL <ivan.novikov301208@gmail.com>
2022-03-02 15:14:18 +0300

    dried the predict_salary func code

--- tools.py
+++ tools.py
@@ -2,14 +2,12 @@ from terminaltables import AsciiTable
 
 
 def predict_salary(salary_from, salary_to):
-    if not salary_from and not salary_to:
-        return None
-    elif not salary_from:
-        return salary_to * 0.8
-    elif not salary_to:
-        return salary_from * 1.2
-    else:
+    if salary_from and salary_to:
         return (salary_from + salary_to) / 2
+    elif salary_to:
+        return salary_to * 0.8
+    elif salary_from:
+        return salary_from * 1.2
 
 
 def get_vacancy_table(vacancies, title):
